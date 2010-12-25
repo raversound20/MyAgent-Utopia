a451c8d5aab7d0a015c55f0a5a92fca05fc8d039
chris <chris@chris-PC>
2010-12-25 17:24:14 +0100

    FirstClass

---
+++ Java_Projekts/SimpleJavaProjekt/src/de/crossbean/media/projects/myAgentUtopia/Mainclass.java
@@ -0,0 +1,13 @@
+package de.crossbean.media.projects.myAgentUtopia;
+
+public class Mainclass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HAllo");
+
+	}
+
+}
